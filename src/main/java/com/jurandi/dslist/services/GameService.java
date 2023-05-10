b17991ea12b559d1c07a291d9ed6627d911e4372
Jurandi Junior <jurandijr20@gmail.com>
2023-05-09 22:02:09 -0300

    SQL, projection, get games by list

--- src/main/java/com/jurandi/dslist/services/GameService.java
+++ src/main/java/com/jurandi/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jurandi.dslist.dto.GameDTO;
 import com.jurandi.dslist.dto.GameMinDTO;
 import com.jurandi.dslist.entities.Game;
+import com.jurandi.dslist.projections.GameMinProjection;
 import com.jurandi.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() { 
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) { 
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
